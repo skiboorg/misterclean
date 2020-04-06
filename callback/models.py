c68aac82a2f3a0eea277f6928d9318b031a125c4
ddnnss <ddnnss.i1@gmail.com>
2020-04-06 16:37:57 +0300

    new pages

--- callback/models.py
+++ callback/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
-
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.template.loader import render_to_string
+import settings
 
 class CallbackOrder(models.Model):
     userName = models.CharField('Имя',max_length=255, blank=False, default='Нет данных')
@@ -19,3 +21,12 @@ class CallbackOrder(models.Model):
         verbose_name = "Форма заказа звонка"
         verbose_name_plural = "Формы заказа звонка"
 
+def callback_ps(sender, instance, **kwargs):
+    msg_html = render_to_string('email/callback.html', {'userName': instance.userName,
+                                                        'userPhone': instance.userPhone,
+                                                        'service': instance.service,
+                                                        'square': instance.square,})
+    send_mail('Заполнена форма на сайте MisterClean ', None, 'no-reply@specsintez-pro.ru', [settings.SEND_TO],
+              fail_silently=False, html_message=msg_html)
+
+post_save.connect(callback_ps, sender=CallbackOrder)
--- misterclean/settings.py
+++ misterclean/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,11 @@ PREPEND_WWW = True
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-
+EMAIL_HOST = settings.SMTP_HOST
+EMAIL_HOST_USER = settings.SMTP_LOGIN
+EMAIL_HOST_PASSWORD = settings.SMTP_PASSWORD
+EMAIL_PORT = settings.SMTP_PORT
+EMAIL_USE_TLS = True
 
 # Application definition
 
